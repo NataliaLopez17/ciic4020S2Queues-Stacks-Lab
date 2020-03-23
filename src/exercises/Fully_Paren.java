0ae4d3549210f30c0dad2244f0e7cef102117853
Natalia Lopez <natalia.lopez17@upr.edu>
2020-03-23 19:30:30 -0400

    asi se queda

--- src/exercises/Fully_Paren.java
+++ src/exercises/Fully_Paren.java
@@ -7,145 +7,118 @@ import ciic4020.stack.Stack;
 
 public class Fully_Paren {
 
-	public static Integer[] theArray;
+	public static Integer[] theArray = new Integer[26];
 
 	public static void main(String[] args) {
 
 		Scanner in = new Scanner(System.in);
 		System.out.println("please enter an expression and its corresponding values: ");
-		String line = null;
-		int i = 0;
-		theArray = new Integer[26];
-		while ((line = in.nextLine()).length() > 0) {
-			if (line.substring(0) == "=") {
+
+		while (true) {
+			String line = in.nextLine();
+			if (line.length() == 0) {
+				break;
+			}
+			if (Character.isAlphabetic(line.charAt(0)) && line.charAt(1) == '=') {
 				String[] valuesArray = line.split("=");
 				int ints = (int) valuesArray[0].charAt(0) - 65;
 				theArray[ints] = Integer.parseInt(valuesArray[1]);
 			}
 			fullyParenthesizedExpression(line);
 		}
-		// fullyParenthesizedExpression(line);
+		System.out.println("The final symbol table is: ");
+		System.out.println("");
+		System.out.println("Variable" + "\t" + "Value");
+		for (int i = 0; i < theArray.length; i++) {
+			if (theArray[i] != null) {
+				Character letter = (char) (i + 65);
+				System.out.println(letter + "\t" + "\t" + theArray[i]);
+			}
+		}
+
 	}
 
 	public static boolean isOperator(char c) {
 		return ((c == '/') || (c == '*') || (c == '+') || (c == '-'));
 	}
 
-	public static boolean isLowerPrecedence(char operand1, char operand2) {
+	public static Integer[] fullyParenthesizedExpression(String infix) {
+		Stack<Character> operatorStack = new ArrayStack<Character>(1);
+		Stack<Integer> operandStack = new ArrayStack<Integer>(1);
+		Stack<Character> parenthesisStack = new ArrayStack<Character>(1);
+		Stack<Integer> resultStack = new ArrayStack<Integer>(1);
 
-		switch (operand1) {
-		case '+':
-		case '-':
-			return !(operand2 == '+' || operand2 == '-');
-
-		case '*':
-		case '/':
-			return operand2 == '(' || operand2 == '[' || operand2 == '{';
-
-		case '(':
-			return true;
-		case '[':
-			return true;
-		case '{':
-			return true;
-
-		default:
-			return false;
-		}
-	}
-
-	public static String fullyParenthesizedExpression(String infix) {
-		Stack<Character> operators = new ArrayStack<Character>(1);
-		Stack<Character> operands = new ArrayStack<Character>(1);
-		Stack<Character> parenthesis = new ArrayStack<Character>(1);
-		Integer[] newArray = theArray;
-
-		char[] infixArr = infix.toCharArray();
-		for (int i = 0; i < infixArr.length; i++) {
-			if (infixArr[i] == '=') {
-				continue;
+		for (int i = 0; i < infix.length(); i++) {
+			if ((infix.charAt(i) != '[' && infix.charAt(i) != '{' && infix.charAt(i) != '(')
+					|| (infix.length() - 1 != ']' && infix.length() - 1 != '}')) {
+				System.out.println(infix + " is invalid");
+				break;
 			}
-		}
-		for (int i = 0; i < infixArr.length; i++) {
-			if (infixArr[i] == ':') {
-				if ((infixArr[0] != '[' && infixArr[0] != '{' && infixArr[0] != '(')
-						|| (infixArr[infixArr.length - 1] != ']' && infixArr[infixArr.length - 1] != '}')) {
-					System.out.println(infix + " is invalid");
-					break;
+			if (infix.charAt(i) == '(' || infix.charAt(i) == '{' || infix.charAt(i) == '[') {
+				parenthesisStack.push(infix.charAt(i));
+			}
+			if ((infix.charAt(i) >= 'A' && (infix.charAt(i) <= 'Z'))) {
+				int ints = (int) infix.charAt(0) - 65;
+				int target = theArray[ints];
+				operandStack.push(target);
+			}
+			if (isOperator(infix.charAt(i))) {
+				operatorStack.push(infix.charAt(i));
+			} else {
+				int ints = 0;
+				int target = 0;
+				if (operatorStack.isEmpty() || operandStack.size() < 2) {
+					if (parenthesisStack.top() != infix.charAt(i)) {
+						System.out.println(infix + " is invalid");
+						break;
+					}
 				}
-				if (infixArr[i] == '(' || infixArr[i] == '{' || infixArr[i] == '[') {
-					parenthesis.push(infixArr[i]);
-				}
-				if ((infixArr[i] >= 'A' && (infixArr[i] <= 'Z'))) {
-					operands.push(infixArr[i]);
-				}
-				if (isOperator(infixArr[i])) {
-					operators.push(infixArr[i]);
-				} else {
-					if (operators.isEmpty() || operands.size() < 2) {
-						if (parenthesis.top() != infixArr[i]) {
-							System.out.println(infix + " is invalid");
-							break;
-						}
-					} else {
-						int op1 = operands.pop();
-						int op2 = operands.pop();
-						char character = operators.pop();
+				if (operandStack.size() >= 2 || !operatorStack.isEmpty()) {
+					if (infix.charAt(i) == ')' || infix.charAt(i) == ']' || infix.charAt(i) == '}') {
+						int op1 = operandStack.pop();
+						int op2 = operandStack.pop();
+						char character = operatorStack.pop();
 
 						switch (character) {
 						case '+':
-							operands.push((char) (op1 + op2));
+							operandStack.push((op1 + op2));
+							resultStack.push((op1 + op2));
+							parenthesisStack.pop();
+							ints = (int) infix.charAt(0) - 65;
+							target = theArray[ints];
 							break;
+
 						case '-':
-							operands.push((char) (op2 - op1));
+							operandStack.push(Math.abs(op2 - op1));
+							resultStack.push((op1 + op2));
+							parenthesisStack.pop();
+							ints = (int) infix.charAt(0) - 65;
+							target = theArray[ints];
 							break;
+
 						case '/':
-							operands.push((char) (op2 / op1));
+							operandStack.push(op2 / op1);
+							resultStack.push((op1 / op2));
+							parenthesisStack.pop();
+							ints = (int) infix.charAt(0) - 65;
+							target = theArray[ints];
 							break;
 						case '*':
-							operands.push((char) (op2 * op1));
+							operandStack.push(op2 * op1);
+							resultStack.push((op1 * op2));
+							parenthesisStack.pop();
+							ints = (int) infix.charAt(0) - 65;
+							target = theArray[ints];
 							break;
-
-						}
-						// parenthesis.pop();
-
-						if (i == infixArr.length - 1 && parenthesis.size() != 0) {
-							System.out.println(infix + " is invalid");
-							break;
-						}
-
-						while (!operators.isEmpty()) {
-							char temp = 0;
-							if (isLowerPrecedence(infixArr[i], operators.top())) {
-								operators.push(infixArr[i]);
-							}
-							if (isLowerPrecedence(operators.top(), infixArr[i])) {
-								temp = operators.pop();
-								operators.push(infixArr[i]);
-							}
 						}
 					}
 				}
-
-				while (infixArr[i] == ')') {
-					if (!operators.isEmpty() && parenthesis.top() != '(') {
-						operands.push(operators.pop());
-					}
-				}
-
-				if (i == infixArr[infixArr.length - 1]) {
-					if (!parenthesis.isEmpty() && !operands.isEmpty()) {
-						operands.push(operators.pop());
-						operands.push(parenthesis.pop());
-
-					}
+				if (i == infix.length() - 1 && parenthesisStack.size() != 0) {
+					System.out.println(infix + " is invalid");
+					break;
 				}
 			}
-			System.out.println(operands.pop());
 		}
-
-		return infix;
-
+		return theArray;
 	}
-
 }
